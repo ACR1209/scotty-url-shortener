4806ae08561ca1e9b9cf86c338a5dfc65ba88ec8
ACR1209 <andrescoronel1209@gmail.com>
2024-11-11 14:32:39 -0500

    <refactor> added digestive-functors and refactored the view into it's own function

--- src/Shortener.hs
+++ src/Shortener.hs
@@ -8,12 +8,40 @@ import Data.IORef (modifyIORef, newIORef, readIORef)
 import Data.Map (Map)
 import qualified Data.Map as M
 import Data.Text (Text)
+import qualified Data.Text as T
 import qualified Data.Text.Lazy as LT
+import Network.URI (parseURI)
 import Network.HTTP.Types (status404)
 import Text.Blaze.Html.Renderer.Text (renderHtml)
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 import Web.Scotty
+import Text.Digestive
+import qualified Text.Digestive.Form as F
+import Text.Digestive.Types
+
+isValidUrl :: Text -> Bool
+isValidUrl input = case parseURI (T.unpack input) of
+    Just _  -> True
+    Nothing -> False
+
+urlForm :: Monad m => F.Form Text m Text
+urlForm = "url" .: check "Invalid URL format" isValidUrl (F.text Nothing)
+
+indexPage :: H.ToMarkup a => Map a Text -> ActionM ()
+indexPage urls = do
+  html $ renderHtml $
+    H.html $
+      H.body $ do
+        H.h1 "Shortener"
+        H.form H.! A.method "post" H.! A.action "/" $ do
+          H.input H.! A.type_ "text" H.! A.name "url"
+          H.input H.! A.type_ "submit"
+        H.table $
+          for_ (M.toList urls) $ \(i, url) ->
+            H.tr $ do
+              H.td (H.toHtml i)
+              H.td (H.text url)
 
 shortener :: IO ()
 shortener = do
@@ -21,18 +49,8 @@ shortener = do
   scotty 3000 $ do
     get "/" $ do
       (_, urls) <- liftIO $ readIORef urlsR
-      html $ renderHtml $
-        H.html $
-          H.body $ do
-            H.h1 "Shortener"
-            H.form H.! A.method "post" H.! A.action "/" $ do
-              H.input H.! A.type_ "text" H.! A.name "url"
-              H.input H.! A.type_ "submit"
-            H.table $
-              for_ (M.toList urls) $ \(i, url) ->
-                H.tr $ do
-                  H.td (H.toHtml i)
-                  H.td (H.text url)
+      indexPage urls
+      
     post "/" $ do
       url <- formParam "url"
       liftIO $ modifyIORef urlsR $
