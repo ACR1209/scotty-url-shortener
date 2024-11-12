74f6980387b4fa98df5f6b59ba61d679605a6de8
ACR1209 <andrescoronel1209@gmail.com>
2024-11-12 15:14:15 -0500

    <feature> integrate PostgreSQL database for URL storage and retrieval

--- src/Shortener.hs
+++ src/Shortener.hs
@@ -4,25 +4,30 @@ module Shortener where
 
 import Control.Monad.IO.Class (MonadIO(liftIO))
 import Data.Foldable (for_)
-import Data.IORef (modifyIORef, newIORef, readIORef)
-import Data.Map (Map)
-import qualified Data.Map as M
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as LT
 import Network.URI (parseURI)
+import Data.Text.Encoding (encodeUtf8)
 import Network.HTTP.Types (status404, status400)
 import Text.Blaze.Html.Renderer.Text (renderHtml)
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 import Web.Scotty
+import Configuration.Dotenv (loadFile, defaultConfig)
+import System.Environment (getEnv)
+import Database.PostgreSQL.Simple
+
+type DbConnection = Connection
+type Url = (Int, Text)
+
 
 isValidUrl :: Text -> Bool
 isValidUrl input = case parseURI (T.unpack input) of
     Just _  -> True
     Nothing -> False
 
-indexPage :: H.ToMarkup a => Map a Text -> ActionM ()
+indexPage :: [Url] -> ActionM ()
 indexPage urls = do
   html $ renderHtml $
     H.html $
@@ -32,34 +37,51 @@ indexPage urls = do
           H.input H.! A.type_ "text" H.! A.name "url"
           H.input H.! A.type_ "submit"
         H.table $
-          for_ (M.toList urls) $ \(i, url) ->
+          for_ urls $ \(urlId, originalUrl) ->
             H.tr $ do
-              H.td (H.toHtml i)
-              H.td (H.text url)
+              H.td (H.toHtml $ show urlId)
+              H.td (H.text originalUrl)
+
+getAllTables :: DbConnection -> IO [Only Text]
+getAllTables conn = query_ conn "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"
+
+getAllUrls :: DbConnection -> IO [Url]
+getAllUrls conn = query_ conn "SELECT id, original FROM url"
+
+insertUrl :: DbConnection -> Text -> IO Int
+insertUrl conn url = do
+  [Only urlId] <- query conn "INSERT INTO url (original) VALUES (?) RETURNING id" (Only url)
+  return urlId
+
+getUrlById :: DbConnection -> Int -> IO (Maybe Text)
+getUrlById conn urlId = do
+  results <- query conn "SELECT original FROM url WHERE id = ?" (Only urlId)
+  return $ case results of
+    [Only url] -> Just url
+    _          -> Nothing
 
 shortener :: IO ()
 shortener = do
-  urlsR <- newIORef (1 :: Int, mempty :: Map Int Text)
+  loadFile defaultConfig
+  dbUrl <- getEnv "DATABASE_URL"
+  conn <- connectPostgreSQL (encodeUtf8 (T.pack dbUrl))
+
   scotty 3000 $ do
     get "/" $ do
-      (_, urls) <- liftIO $ readIORef urlsR
+      urls <- liftIO $ getAllUrls conn
       indexPage urls
-      
+
     post "/" $ do
       url <- formParam "url"
-      if not (isValidUrl url)
+      if not $ isValidUrl url
         then raiseStatus status400 "invalid url"
         else do
-          liftIO $ modifyIORef urlsR $
-            \(i, urls) ->
-              (i + 1, M.insert i url urls)
+          _ <- liftIO $ insertUrl conn url
           redirect "/"
           
     get "/:n" $ do
       n <- captureParam "n"
-      (_, urls) <- liftIO $ readIORef urlsR
-      case M.lookup n urls of
-        Just url ->
-          redirect (LT.fromStrict url)
-        Nothing ->
-          raiseStatus status404 "not found"
+      url <- liftIO $ getUrlById conn n
+      case url of
+        Just u  -> redirect (LT.fromStrict u)
+        Nothing -> raiseStatus status404 "url not found" 
